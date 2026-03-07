8cc846c3bc8d74eda4331a67b944c2d69251ba1d
Kishor-017 <kishorkumark0017@gmail.com>
2026-03-07 12:12:34 +0530

    Add countEven method to count even sums of digits
    Implement countEven method to count even digit sums.

---
+++ Day_13/Leetcode2180.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countEven(self, num: int) -> int:
+        count=0
+        for i in range(1,num+1):
+            sum=0
+            while i>0:
+                sum+=i%10
+                i=i//10
+            if sum%2==0:
+                count+=1
+            else:
+                continue
+        return count
