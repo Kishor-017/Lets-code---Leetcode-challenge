1fec44d7d11889ba4910b47bf4b2a416f9870b08
Kishor-017 <kishorkumark0017@gmail.com>
2026-03-18 18:43:43 +0530

    Implement string restoration in Shufflestring.py

---
+++ Day_23/Shufflestring.py
@@ -0,0 +1,6 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        a=[""]*len(s)
+        for i in range(len(indices)):
+            a[indices[i]]=s[i]
+        return ''.join(a)
