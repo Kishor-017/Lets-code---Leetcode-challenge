67ff6a74896f178dea2f314e235a6aea5f9a03b1
Kishor-017 <kishorkumark0017@gmail.com>
2026-04-09 14:59:40 +0530

    Implement findMaxAverage method in Solution class

---
+++ Day_37/Leetcode643.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        total=sum(nums[0:k])
+        max_sum=total
+        for i in range(k,len(nums)):
+            total+=nums[i]-nums[i-k]
+            max_sum=max(max_sum,total)
+        return max_sum/k
